8d56d65643d620035bf1b4f422e9ddf39730a4d8
Amit Kumar Raikwar <amitkumarraikwar92@gmail.com>
2026-07-27 00:17:52 +0530

    fix: add external image domains to Next.js config to fix 400 errors

--- novaedge-frontend/next.config.mjs
+++ novaedge-frontend/next.config.mjs
@@ -12,6 +12,34 @@ const nextConfig = {
         protocol: "https",
         hostname: "res.cloudinary.com",
       },
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "img.youtube.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "*.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.gravatar.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
     ],
   },
   typescript: {
